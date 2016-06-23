44593de7515a09c61adf518a720e5d6c4e3475d6
Ranjeet Rai <ranjeetrai42@gmail.com>
2016-06-23 12:30:00 +0530

    initial commit

---
+++ polls/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Question
+from django.template import loader
+
+ 
+
+# Create your views here.
+def ranjeet(request):
+	return HttpResponse("Hello Ranjeet how are you???")
+def detail(request, question_id):
+	return HttpResponse("You arelooking at the question %s " %question_id)
+
+def results(request, question_id):
+	response="You arelooking at the resutl of question %s" 
+	return HttpResponse(response %question_id)
+
+def vote(request, question_id):
+	return HttpResponse("You are votingon question %s" %question_id)
+
+def index(request):
+	latest_question_list= Question.objects.order_by('-pub_date')[:5]
+	template = loader.get_template('polls/index.html')
+	context = { 'latest_question_list': latest_question_list , } 
+	return HttpResponse(template.render(context,request))
